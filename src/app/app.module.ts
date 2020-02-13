eafd0d786c6ed4a674643b262e1714e7d73955e6
Goncalves Daniel <rgdaniel@indracompany.com>
2020-02-13 14:05:05 -0300

    commiting the components: home,form,header,post-principal,post-secundário

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FormComponent } from './form/form.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { PostPrincipalComponent } from './home/post-principal/post-principal.component';
+import { PostSecundarioComponent } from './home/post-secundario/post-secundario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FormComponent,
+    HeaderComponent,
+    PostPrincipalComponent,
+    PostSecundarioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/post-principal/post-principal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-principal',
+  templateUrl: './post-principal.component.html',
+  styleUrls: ['./post-principal.component.css']
+})
+export class PostPrincipalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
